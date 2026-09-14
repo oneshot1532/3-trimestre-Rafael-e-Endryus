e362aa87ac872ce0e2910c05d194753d5bc16002
oneshot1532 <rafael.holler@escola.pr.gov.br>
2026-09-14 12:22:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,48 +1,59 @@
 criarCartao(
+    'Programação',
+    'O que é Javascript?',
+    'Uma linguagem de programação'
+)
+criarCartao(
+    'Matemática',
+    'O que é um Poliedro?',
+    'Uma forma geométrica 3D'
+)
+criarCartao(
+    'Português',
+    'Oque é um Adjetivo?',
+    'A classe gramatical que dá características ás palavras'
+)
+criarCartao(
+    'Geografia',
+    'Qual é a capital da Alemanha?',
+    'Berlim'
+)
+criarCartao(
+    'História',
+    'QUando foi a independência do Brasil?',
+    '7 de Setembro de 1822'
+)
+criarCartao(
     'Categoria',
     'Pergunta',
     'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
-)criarCartao(
+)
+criarCartao(
+    'Filosofia',
+    'Qual o filósofo que teve a teoria do Mundo das Ideias?',
+    'Platão'
+)
+criarCartao(
+    'Quimíca',
+    'Quantos elétrons o Hidrogênio tem na sua camada de valência?',
+    'Apenas 1'
+)
+criarCartao(
+    'Física',
+    'Força é igual á...?',
+    'Massa x Velocidade'
+)
+criarCartao(
+    'Artes',
+    'Qual o movimento artístico que desafiava o estilo tradicional?',
+    'Modernismo'
+)
+criarCartao(
+    'ED. Financeira',
+    'Qual o método que aprendemos nessa matéria?',
+    'Método 50-30-20'
+)
+criarCartao(
     'Categoria',
     'Pergunta',
     'Resposta'
